41696cd8b53218cb13f4a72e9e006d7b615e3fc6
baysicinglish <dbeazley.400@gmail.com>
2024-12-01 17:55:36 +0000

    Part one solution

---
+++ day_1/historian_hysteria.py
@@ -0,0 +1,21 @@
+def get_ids_by_group(location_ids: list[list[str]]):
+    group_one_ids = sorted([int(ids[0]) for ids in location_ids])
+    group_two_ids = sorted([int(ids[1]) for ids in location_ids])
+
+    return group_one_ids, group_two_ids
+
+
+def solve_part_one(group_one_ids: list[int], group_two_ids: list[int]):
+    total_distance = 0
+    for location in range(len(location_ids)):
+        total_distance += abs(group_two_ids[location] - group_one_ids[location])
+
+    return total_distance
+
+
+if __name__ == "__main__":
+    with open("day_1/inputs/input.txt") as file:
+        location_ids = [line.split() for line in file]
+        group_one_ids, group_two_ids = get_ids_by_group(location_ids)
+
+        print(solve_part_one(group_one_ids, group_two_ids))
