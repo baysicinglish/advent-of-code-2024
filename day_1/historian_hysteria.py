bbe4b7a67edc6324c58e37690d0d15a2d4139907
baysicinglish <dbeazley.400@gmail.com>
2024-12-01 18:10:48 +0000

    Part two solution

--- day_1/historian_hysteria.py
+++ day_1/historian_hysteria.py
@@ -13,9 +13,18 @@ def solve_part_one(group_one_ids: list[int], group_two_ids: list[int]):
     return total_distance
 
 
+def solve_part_two(group_one_ids: list[int], group_two_ids: list[int]):
+    similarity_score = 0
+    for location_id in group_one_ids:
+        similarity_score += location_id * group_two_ids.count(location_id)
+
+    return similarity_score
+
+
 if __name__ == "__main__":
     with open("day_1/inputs/input.txt") as file:
         location_ids = [line.split() for line in file]
         group_one_ids, group_two_ids = get_ids_by_group(location_ids)
 
         print(solve_part_one(group_one_ids, group_two_ids))
+        print(solve_part_two(group_one_ids, group_two_ids))
