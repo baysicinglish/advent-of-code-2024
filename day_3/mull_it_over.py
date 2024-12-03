ef22e4cc91c4a7579e8d71b6569010b5f477ee3c
baysicinglish <dbeazley.400@gmail.com>
2024-12-03 20:58:35 +0000

    Part two solution
    Mental note: beware end of line characters

--- day_3/mull_it_over.py
+++ day_3/mull_it_over.py
@@ -10,7 +10,18 @@ def scan_memory(line_of_memory: str) -> int:
     return total
 
 
+def remove_disabled_memory(line_of_memory: str) -> str:
+    return re.sub(r"don't\(\).*?(do\(\)|$)", "<3", line_of_memory)
+
+
 if __name__ == "__main__":
-    with open("day_3/inputs/input.txt") as corrupted_memory:
-        result = sum([scan_memory(line) for line in corrupted_memory])
-        print(result)
+    with open("day_3/inputs/input.txt") as memory:
+        corrupted_memory = memory.readlines()
+
+    corrupted_memory = "<3".join([line.strip() for line in corrupted_memory])
+    result = scan_memory(corrupted_memory)
+    print(result)
+
+    enabled_memory = remove_disabled_memory(corrupted_memory)
+    result = scan_memory(enabled_memory)
+    print(result)
