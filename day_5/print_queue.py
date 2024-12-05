597df3d26ff93c96761e1e84d15e6613ef580ba7
baysicinglish <dbeazley.400@gmail.com>
2024-12-05 14:54:55 +0000

    Prefactor before part two

--- day_5/print_queue.py
+++ day_5/print_queue.py
@@ -1,30 +1,36 @@
 from collections import defaultdict
 
+
+def get_updates_and_prerequisites(print_instructions):
+    prerequisites = defaultdict(set)
+    for line in print_instructions:
+        if not (prerequisite := line.strip()):
+            break
+
+        after, before = prerequisite.split("|")
+        prerequisites[before].add(int(after))
+
+    updates = [[int(page) for page in update.strip().split(",")] for update in print_instructions]
+    return updates, prerequisites
+
+
+def is_in_order(update, prerequisites):
+    too_late = set()
+
+    for page in update:
+        if page in too_late:
+            return False
+
+        too_late = too_late.union(prerequisites[str(page)])
+
+    return True
+
+
 if __name__ == "__main__":
     with open("day_5/inputs/input.txt") as print_instructions:
-        prerequisites = defaultdict(set)
-        for line in print_instructions:
-            if not (prerequisite := line.strip()):
-                break
+        updates, prerequisites = get_updates_and_prerequisites(print_instructions)
 
-            after, before = prerequisite.split("|")
-            prerequisites[before].add(int(after))
-
-        updates = [[int(page) for page in update.strip().split(",")] for update in print_instructions]
-
-    middle_page_sum = 0
-
-    for update in updates:
-        midpoint = len(update) // 2
-        middle_page_sum += update[midpoint]
-        too_late = set()
-
-        for page in update:
-            if page in too_late:
-                middle_page_sum -= update[midpoint]
-                break
-
-            too_late = too_late.union(prerequisites[str(page)])
-
-    print(middle_page_sum)
-
+    middle_page_of_ordered_updates = [
+        update[len(update) // 2] for update in updates if is_in_order(update, prerequisites)
+    ]
+    print(sum(middle_page_of_ordered_updates))
